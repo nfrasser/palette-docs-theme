615b9d59bd07506208a3070da28d56b159bb14a7
Nick Frasser <nfrasser@palettegear.com>
2018-07-09 14:11:58 -0400

    Add documentation.js as dependency

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
+'use strict';
+
 var fs = require('fs'),
-  path = require('path'),
-  File = require('vinyl'),
-  vfs = require('vinyl-fs'),
-  _ = require('lodash'),
-  concat = require('concat-stream'),
-  GithubSlugger = require('github-slugger'),
-  createFormatters = require('../').util.createFormatters,
-  LinkerStack = require('../').util.LinkerStack,
-  hljs = require('highlight.js');
+    path = require('path'),
+    File = require('vinyl'),
+    vfs = require('vinyl-fs'),
+    _ = require('lodash'),
+    concat = require('concat-stream'),
+    GithubSlugger = require('github-slugger'),
+    createFormatters = require('documentation').util.createFormatters,
+    LinkerStack = require('documentation').util.LinkerStack,
+    hljs = require('highlight.js');
 
 function isFunction(section) {
-  return (
-    section.kind === 'function' ||
-    (section.kind === 'typedef' &&
-      section.type.type === 'NameExpression' &&
-      section.type.name === 'Function')
-  );
+  return section.kind === 'function' || section.kind === 'typedef' && section.type.type === 'NameExpression' && section.type.name === 'Function';
 }
 
-module.exports = function(
-  comments: Array<Comment>,
-  config: DocumentationConfig
-) {
-  var linkerStack = new LinkerStack(config).namespaceResolver(
-    comments,
-    function(namespace) {
-      var slugger = new GithubSlugger();
-      return '#' + slugger.slug(namespace);
-    }
-  );
+module.exports = function (comments, config) {
+  var linkerStack = new LinkerStack(config).namespaceResolver(comments, function (namespace) {
+    var slugger = new GithubSlugger();
+    return '#' + slugger.slug(namespace);
+  });
 
   var formatters = createFormatters(linkerStack.link);
 
@@ -63,12 +54,7 @@ module.exports = function(
         return prefix + section.name + formatters.parameters(section) + returns;
       },
       md(ast, inline) {
-        if (
-          inline &&
-          ast &&
-          ast.children.length &&
-          ast.children[0].type === 'paragraph'
-        ) {
+        if (inline && ast && ast.children.length && ast.children[0].type === 'paragraph') {
           ast = {
             type: 'root',
             children: ast.children[0].children.concat(ast.children.slice(1))
@@ -87,47 +73,23 @@ module.exports = function(
     }
   };
 
-  sharedImports.imports.renderSectionList = _.template(
-    fs.readFileSync(path.join(__dirname, 'section_list._'), 'utf8'),
-    sharedImports
-  );
-  sharedImports.imports.renderSection = _.template(
-    fs.readFileSync(path.join(__dirname, 'section._'), 'utf8'),
-    sharedImports
-  );
-  sharedImports.imports.renderNote = _.template(
-    fs.readFileSync(path.join(__dirname, 'note._'), 'utf8'),
-    sharedImports
-  );
-  sharedImports.imports.renderParamProperty = _.template(
-    fs.readFileSync(path.join(__dirname, 'paramProperty._'), 'utf8'),
-    sharedImports
-  );
+  sharedImports.imports.renderSectionList = _.template(fs.readFileSync(path.join(__dirname, 'section_list._'), 'utf8'), sharedImports);
+  sharedImports.imports.renderSection = _.template(fs.readFileSync(path.join(__dirname, 'section._'), 'utf8'), sharedImports);
+  sharedImports.imports.renderNote = _.template(fs.readFileSync(path.join(__dirname, 'note._'), 'utf8'), sharedImports);
+  sharedImports.imports.renderParamProperty = _.template(fs.readFileSync(path.join(__dirname, 'paramProperty._'), 'utf8'), sharedImports);
 
-  var pageTemplate = _.template(
-    fs.readFileSync(path.join(__dirname, 'index._'), 'utf8'),
-    sharedImports
-  );
+  var pageTemplate = _.template(fs.readFileSync(path.join(__dirname, 'index._'), 'utf8'), sharedImports);
 
   // push assets into the pipeline as well.
   return new Promise(resolve => {
-    vfs.src([__dirname + '/assets/**'], { base: __dirname }).pipe(
-      concat(function(files) {
-        resolve(
-          files.concat(
-            new File({
-              path: 'index.html',
-              contents: new Buffer(
-                pageTemplate({
-                  docs: comments,
-                  config
-                }),
-                'utf8'
-              )
-            })
-          )
-        );
-      })
-    );
+    vfs.src([__dirname + '/assets/**'], { base: __dirname }).pipe(concat(function (files) {
+      resolve(files.concat(new File({
+        path: 'index.html',
+        contents: new Buffer(pageTemplate({
+          docs: comments,
+          config
+        }), 'utf8')
+      })));
+    }));
   });
 };
